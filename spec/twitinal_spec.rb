4ae9cd496e5086f918d5f4294435df93fa8cbf6d
ujihisa <ujihisa@gmail.com>
2009-08-28 01:08:44 -0700

    Wrote a spec

--- spec/twitinal_spec.rb
+++ spec/twitinal_spec.rb
@@ -1,7 +1,18 @@
 $: << File.dirname(__FILE__) + '/..'
 require 'lib/twitinal'
 
+def internet_connected
+  @internet_connected ||= system('ping -c 1 -t 1 google.ca >&/dev/null')
+end
+
 describe 'Twitinal.get' do
   it 'attempts to access twitter' do
+    if internet_connected
+      a = Twitinal.get('ujm')
+      a.should be_instance_of Array
+      a.size.should == 20
+    else
+      pending
+    end
   end
 end
